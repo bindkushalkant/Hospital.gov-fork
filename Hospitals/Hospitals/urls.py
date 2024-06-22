db14c5be3be19c6079cd3f6509a5dd33bc7c157e
Kushal kant Bind <149178326+Bindkushal@users.noreply.github.com>
2024-06-22 17:27:31 +0000

    Sat, Jun 22, 2024, 10:57 PM +05:30

--- Hospitals/Hospitals/urls.py
+++ Hospitals/Hospitals/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from djan
+from django.conf import settings
+from django.conf
 
 urlpatterns = [
     path("admin/", admin.site.urls),
