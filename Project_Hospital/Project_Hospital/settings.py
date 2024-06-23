e3512c8429f7cbdcbf091144592aff374a8ab102
Kushal kant Bind <149178326+Bindkushal@users.noreply.github.com>
2024-06-23 09:47:14 +0000

    Sun, Jun 23, 2024, 3:17 PM +05:30

--- Project_Hospital/Project_Hospital/settings.py
+++ Project_Hospital/Project_Hospital/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "/images/"
-STATIFILES_DIR =
+STATIFILES_DIR = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
