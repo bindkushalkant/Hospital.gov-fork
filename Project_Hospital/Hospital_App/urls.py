88754b9c7110b52fdda308a87e0875dcc37c032d
Kushal kant Bind <149178326+Bindkushal@users.noreply.github.com>
2024-06-23 09:05:40 +0000

    Sun, Jun 23, 2024, 2:35 PM +05:30

--- Project_Hospital/Hospital_App/urls.py
+++ Project_Hospital/Hospital_App/urls.py
@@ -1,6 +1,7 @@
- from django.urls import path 
+ from django.urls import path
  from . import views
 
  urlpatterns = [
      path("", views.index, name="index")
+     path('Hospital_App/', include("Hospital_App.urls"))
  ]
