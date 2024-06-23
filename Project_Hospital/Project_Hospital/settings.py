716c1e2dd45fdc9df0e3c6f20e4130723d558b97
Kushal kant Bind <149178326+Bindkushal@users.noreply.github.com>
2024-06-23 09:27:31 +0000

    Sun, Jun 23, 2024, 2:57 PM +05:30

--- Project_Hospital/Project_Hospital/settings.py
+++ Project_Hospital/Project_Hospital/settings.py
@@ -118,12 +118,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# settings.py
 
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ...
 
@@ -133,8 +128,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# Make sure you have this setting to collect static files into one directory for production
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
